7d3349424160070de3e4088572d05fee93f10150
seohyung kang <leejaehyun600319@gmail.com>
2024-06-28 13:01:38 +0900

    feat: rebuild

---
+++ release.config.js
@@ -0,0 +1,18 @@
+export default {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
