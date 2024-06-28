98227a1e5f0b7330d385caa24a798da2c1f9bff5
seohyung kang <leejaehyun600319@gmail.com>
2024-06-28 18:17:53 +0900

    build: rebuild

--- release.config.js
+++ release.config.js
@@ -1,11 +1,31 @@
 export default {
   branches: ["main"],
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/npm",
-    "@semantic-release/github",
-    "@semantic-release/changelog",
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        releaseRules: [
+          { scope: "breaking", release: "major" },
+          { scope: "no-release", release: false },
+          { type: "feat", release: "minor" },
+          { type: "fix", release: "patch" },
+          { type: "perf", release: "patch" },
+          { type: "docs", release: "patch" },
+          { type: "refactor", release: "patch" },
+          { type: "revert", release: "patch" },
+          { type: "style", release: "patch" },
+          { type: "build", release: false },
+          { type: "chore", release: false },
+          { type: "ci", release: false },
+          { type: "test", release: false },
+        ],
+      },
+    ],
+    ["@semantic-release/release-notes-generator"],
+    ["@semantic-release/npm"],
+    ["@semantic-release/github"],
+    ["@semantic-release/changelog"],
     [
       "@semantic-release/git",
       {
